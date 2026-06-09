0cc341f6891c812989b0c42ce0229a23cf6285c1
Maisha Islam Mim <140744060+maishaislam188@users.noreply.github.com>
2026-06-09 15:45:15 +0600

    Implement substring occurrence counter
    Count occurrences of 'ptr' in 'str' with overlapping support.

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,21 @@
+str = input()
 
+ptr = input()
+
+cnt = 0
+
+for i in range(len(str)):
+    flag = 0
+    for j in range(len(ptr)):
+         if i + j < len(str):
+             if str[i+j] != ptr[j]:
+                 flag = 1
+                 
+         if i + j > len(str):
+             flag = 1        
+                 
+    if flag == 0:
+        cnt += 1
+ 
+        
+print(cnt)
