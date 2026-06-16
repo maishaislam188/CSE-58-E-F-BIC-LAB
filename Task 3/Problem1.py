be8bcfa715cef1e295c9e27549ff906242e4d7fb
Maisha Islam Mim <140744060+maishaislam188@users.noreply.github.com>
2026-06-16 16:09:19 +0600

    Add function to find frequent words with mismatches

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,37 @@
+def frequent_words_with_mismatch(text, k, d):
+    
+    patterns = {}
+    max_cnt = 0
+    
+    for i in range(len(text) - k + 1):
+        pattern = text[i:i+k]
+        
+        count = 0
+        
+        for j in range(len(text) - k + 1):
+            substring = text[j:j+k]
+            
+            diff = 0
+            for x in range(k):
+                if pattern[x] != substring[x]:
+                    diff += 1
+                    
+            if diff <= d:
+                count += 1
+                
+        patterns[pattern] = count
+        
+        if count > max_cnt:
+            max_cnt = count
+            
+    for pattern in patterns:
+        if patterns[pattern] == max_cnt:
+            print(pattern, end=" ")
+            
+text = input("Enter DNA String: ")
+k = int(input("Enter k: "))
+d = int(input("Enter d: "))
 
+frequent_words_with_mismatch(text, k, d)                                    
+        
+        
